f5643c58b31cc02f654e3576a2efa399593bfa58
Sarah Haddix <sarahhaddix@sarahs-mbp-2.wireless-1x.unc.edu>
2024-02-23 12:55:26 -0500

    unfinished, have to fix some problems

---
+++ instagramdemo/instagramdemo/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  instagramdemo
+//
+//  Created by Sarah Haddix on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                HStack {
+                    Image("cyberpunkdolphin")
+                        .resizable()
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .frame(width: 50, height: 50)
+                    Text("Cyberpunk_Dolphin")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                    Spacer()
+                    Image(systemName: "ellipsis")
+                        .resizable()
+                        .aspectRatio(nil, contentMode: .fit)
+                        .frame(width: 16, height: 16)
+                        .foregroundColor(.white)
+                    
+                }
+                .padding(.horizontal, 8)
+                Image("cyberpunkdolphin")
+                    .resizable()
+                    .frame(height: 400)
+                    .scaledToFill()
+                HStack {
+                    //TODO: Simplify
+                    //TODO: Fix spacing
+                    Image(systemName: "heart")
+                        .resizable()
+                        .aspectRatio(nil, contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.white)
+                        .padding(.trailing, 2)
+                    Image(systemName: "message")
+                        .resizable()
+                        .aspectRatio(nil, contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 2)
+                    Image(systemName: "paperplane")
+                        .resizable()
+                        .aspectRatio(nil, contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 2)
+                    Spacer()
+                    Image(systemName: "bookmark")
+                        .resizable()
+                        .aspectRatio(nil, contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.white)
+                    
+                }
+                .padding(.horizontal, 8)
+                //TODO: Fix spacing
+                HStack{
+                    VStack (alignment: .leading, content: {
+                        Text("1,000,000 Likes")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                        Text("**Cyberpunk_Dolphin** Tough match last night. ")
+                            .foregroundStyle(.white)
+                            .font(.footnote)
+                        Text("Last night")
+                            .font(.caption)
+                            .foregroundStyle(.white
+                            )
+                    })
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ instagramdemo/instagramdemo/instagramdemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  instagramdemoApp.swift
+//  instagramdemo
+//
+//  Created by Sarah Haddix on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct instagramdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
